ced609bda19e5597b75c0e2e52875135f63d9f45
Hemanth Reddy <hemanth5636@gmail.com>
2026-07-11 01:56:41 -0700

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int result = 0;
+        int left = 0;
+        int right = 1;
+        int minSoFar = prices[0];
+
+        while (right < prices.length) {
+            while (left < right && prices[left] > prices[right]) left++;
+
+            if (left != right) {
+                result = Math.max(result, prices[right] - prices[left]);
+            }
+            right++;
+        }
+
+        return result;
+    }
+}
