f22ecc39f8962a7c612e4beffaa598840d30e48b
Hemanth Reddy <hemanth5636@gmail.com>
2026-08-06 11:54:59 -0700

    Add: eating-bananas - submission-1

---
+++ Data Structures & Algorithms/eating-bananas/submission-1.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        if (h < piles.length) return -1;
+
+        int l = 1;
+        int r = 0;
+
+        for (int p : piles) {
+            r = Math.max(r, p);
+        }
+
+        int res = r;
+        while ( l <= r) {
+            int m = l + (r - l) / 2;
+            int cur = 0;
+            for (int p : piles) {
+                cur += p / m;
+                if ( p % m != 0) cur++;
+                if (cur > h) break;
+            }
+
+            if (cur <= h) res = m;
+
+            if (cur > h) {
+                l = m + 1;
+            } else {
+                r = m - 1;
+            }
+        }
+
+        return res;
+    }
+}
