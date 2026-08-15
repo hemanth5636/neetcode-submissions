491913ec6fc73f9825c9f2d2d20beb37b41c31b2
Hemanth Reddy <hemanth5636@gmail.com>
2026-08-15 16:19:50 -0700

    Add: course-schedule-iv - submission-6

---
+++ Data Structures & Algorithms/course-schedule-iv/submission-6.java
@@ -0,0 +1,47 @@
+class Solution {
+    // memorized dfs approach
+    int[][] dp;
+    List<Integer>[] graph;
+
+    public List<Boolean> checkIfPrerequisite(int nc, int[][] preq, int[][] qs) {
+        dp = new int[nc][nc];
+        graph = new ArrayList[nc];
+
+        for (int i = 0; i < nc; i++) {
+            graph[i] = new ArrayList<>();
+            Arrays.fill(dp[i], -1);
+        }
+
+        for (int[] p : preq) {
+            int prequ = p[0];
+            int course = p[1];
+            graph[course].add(prequ);
+            dp[course][prequ] = 1;
+        }
+
+        List<Boolean> result = new ArrayList<>();
+        for (int[] q : qs) {
+            int course = q[1];
+            int prequ = q[0];
+            result.add(dfs(course, prequ));
+        }
+
+        return result;
+    }
+
+    private boolean dfs(int course, int prequisite) {
+        if (dp[course][prequisite] != -1) {
+            return dp[course][prequisite] == 1;
+        }
+
+        for (int pre : graph[course]) {
+            if (pre == prequisite || dfs(pre, prequisite)) {
+                dp[course][prequisite] = 1;
+                return true;
+            }
+        }
+
+        dp[course][prequisite] = 0;
+        return false;
+    }
+}
